b0ae10a8333250fea21c079acea041a226cb4b58
Alexander <60126466+Ezharjan@users.noreply.github.com>
2023-09-01 01:38:54 +0800

    Create nodejs.sh

---
+++ nodejs.sh
@@ -0,0 +1,9 @@
+# Some packages might not be correctly installed if the node version is too low.
+
+wget https://nodejs.org/dist/v18.14.2/node-v18.14.2-linux-x64.tar.xz
+
+sudo tar -C /usr/local --strip-components 1 -xJf node-v18.14.2-linux-x64.tar.xz
+
+
+## Reference
+# https://itslinuxfoss.com/update-nodejs-version-ubuntu-command-line/
