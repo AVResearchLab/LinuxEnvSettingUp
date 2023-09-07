3a889b6bc1b0e1dd016775428b1934ce1bb2d391
Alexander <60126466+Ezharjan@users.noreply.github.com>
2023-09-07 11:09:31 +0800

    Create gpu_driver.sh

---
+++ gpu_driver.sh
@@ -0,0 +1,11 @@
+# For Linux Ubuntu only
+
+sudo ubuntu-drivers list
+
+sudo ubuntu-drivers install nvidia:525
+
+sudo ubuntu-drivers install --gpgpu
+
+sudo ubuntu-drivers install --gpgpu nvidia:525-server
+
+### Reference: https://help.ubuntu.com/community/NvidiaDriversInstallation
