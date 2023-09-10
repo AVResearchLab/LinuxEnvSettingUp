25aa86ce95cb0e43b62a467c06959ed43712bb3a
Alexander <60126466+Ezharjan@users.noreply.github.com>
2023-09-10 15:47:34 +0800

    Update gpu_driver.sh

--- gpu_driver.sh
+++ gpu_driver.sh
@@ -9,3 +9,5 @@ sudo ubuntu-drivers install --gpgpu
 sudo ubuntu-drivers install --gpgpu nvidia:525-server
 
 ### Reference: https://help.ubuntu.com/community/NvidiaDriversInstallation
+### https://blog.csdn.net/weixin_51706755/article/details/122631295
+
