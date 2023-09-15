1234a9f97b3034753adc98fcaccc0c0b9783b628
Alexander <60126466+Ezharjan@users.noreply.github.com>
2023-09-15 15:24:26 +0800

    Create gnome.sh

---
+++ gnome.sh
@@ -0,0 +1,8 @@
+sudo apt-get install gnome-shell
+sudo apt-get install ubuntu-gnome-desktop
+sudo apt-get install unity-tweak-tool
+sudo apt-get install gnome-tweak-tool
+
+# Windows style desktop display
+sudo apt-get install gnome-shell-extension-dash-to-panel
+# Reference: https://github.com/home-sweet-gnome/dash-to-panel/wiki/Installation
