4078087477cf5398a6ad6f2d0a1ec1db0f54b210
Ezharjan <ezharjan@outlook.com>
2023-11-28 19:37:12 +0800

    auto commit at ostime(171432) via gitPusher

--- cuda.sh
+++ cuda.sh
@@ -3,43 +3,30 @@
 nvidia-smi
 
 # Then install CUDA
-sudo apt install nvidia-cuda-toolkit
 # Check CUDA Installation
 nvcc --version
-
-# If you want to remove original Nvidia driver and reinstall, run the following commands:
-sudo rm -r /var/lib/dkms/nvidia
-sudo dpkg -P --force-all $(dpkg -l | grep "nvidia-" | grep -v lib | awk '{print $2}')
-sudo ubuntu-drivers autoinstall
-sudo ubuntu-drivers install
-ubuntu-drivers devices  # this should output lots of the drivers' list
-sudo apt-get install nvidia-driver-535-open # this can install the relevant nvidia driver
-# reboot
 
 
 # Note that the NVidia Driver and its CUDA version can be higher, as you may output higher driver version like 525 with cuda 12.0.
 # But it doesn't matter for you to install specific cuda version inside conda environment using the last line of code in this script.
 # Namely, you can have only 1 driver version with a highest compatible CUDA version (that's what you see when runing `nvidia-smi`) while you can have multiple CUDA versions in different conda virtual environments. The specific CUDA version can be printed via `nvcc --version` command.
-
-
-
-################################################################################################################
-################################################################################################################
-################################################################################################################
-################################################################################################################
-
-# If you want to downgrade CUDA, try finding the suitable CUDA version for your GPU, eg. CUDA-11.8 for RTX3090, then install from the offcial webpage after conducting the following commands:
 sudo apt --purge remove "cuda*"
 sudo apt --purge remove "nvidia*"
 sudo rm -rf /usr/local/cuda*
 sudo apt-get autoremove && sudo apt-get autoclean
 # reboot
 
+# Install all the ubuntu drivers after removing some broken packages
+sudo ubuntu-drivers autoinstall
+sudo ubuntu-drivers install
+
+# Install Nvidia drivers manually downloaded from "https://www.nvidia.com/download/driverResults.aspx/213194/en-us/"
+# eg: ./NVIDIA-Linux-x86_64-535.129.03.run
+
 # Install the downloaded CUDA with *.run file from "https://developer.nvidia.com/cuda-downloads?target_os=Linux"
+
 # reboot, then check to see if NVidia is properly installed
-nvidia-smi
 # check if nvcc is properly installed
-nvcc --version
 # if not, use conda in your environment to install nvcc as follows:
 conda activate your_venv # remember to activate your virtual environment
 conda install -c "nvidia/label/cuda-11.8.0" cuda-nvcc -y
