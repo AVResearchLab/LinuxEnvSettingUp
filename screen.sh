456ad37b5ecef74dabd38f4b12075b7d113660e4
Ezharjan <ezharjan@outlook.com>
2023-12-28 10:25:19 +0800

    auto commit at ostime(730319) via gitPusher

---
+++ screen.sh
@@ -0,0 +1,7 @@
+# Screenshot tool
+sudo apt install gnome-screenshot
+# Next time you can user [Shift]+[PrintScreen] to choose a specific area on the screen for capturing
+
+# Screen recorder
+sudo add-apt-repository ppa:obsproject/obs-studio
+sudo apt install obs-studio
