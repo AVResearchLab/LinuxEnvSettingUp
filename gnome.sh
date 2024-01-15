dc88d29b42ca89b98c9a37f69a0576ee3e60f9a1
Ezharjan <ezharjan@outlook.com>
2024-01-15 16:25:30 +0800

    auto commit at ostime(307130) via gitPusher

--- gnome.sh
+++ gnome.sh
@@ -1,15 +1,15 @@
-sudo apt-get install gnome-shell
-sudo apt-get install ubuntu-gnome-desktop
-sudo apt-get install unity-tweak-tool
-sudo apt-get install gnome-tweak-tool
+sudo apt-get install gnome-shell -y
+sudo apt-get install ubuntu-gnome-desktop -y
+sudo apt-get install unity-tweak-tool -y
+sudo apt-get install gnome-tweak-tool -y
 
 
 # Windows style desktop display
-sudo apt-get install gnome-shell-extension-dash-to-panel
+sudo apt-get install gnome-shell-extension-dash-to-panel -y
 # Reference: https://github.com/home-sweet-gnome/dash-to-panel/wiki/Installation
 
 
-sudo apt install gnome-shell=3.36.4-1ubuntu1~20.04.2 gnome-shell-common=3.36.4-1ubuntu1~20.04.2 gnome-shell-extension-prefs=3.36.4-1ubuntu1~20.04.2
+sudo apt install gnome-shell=3.36.4-1ubuntu1~20.04.2 gnome-shell-common=3.36.4-1ubuntu1~20.04.2 gnome-shell-extension-prefs=3.36.4-1ubuntu1~20.04.2 -y
 # Install via Ubuntu Software
 # Starts
 gnome-tweaks
