af49efabaf661959c4be600b2c89812ba3ad1dc3
Ezharjan <ezharjan@outlook.com>
2024-08-03 17:41:16 +0800

    auto commit at ostime(678076) via gitPusher

--- git.sh
+++ git.sh
@@ -3,3 +3,10 @@ git config --global credential.helper store
 git config --global user.email "ezharjan@outlook.com"
 # git config --global user.name "Your Name"
 git config --global user.name "Ezharjan"
+
+# only for current repository
+# project overrides global and global overrides system
+git config --local credential.helper store
+git config --local user.email "ezharjan@outlook.com"
+git config --local user.name "Ezharjan"
+
